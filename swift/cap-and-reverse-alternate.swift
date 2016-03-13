b2a896d63d39784d625ef1e38f79a8c099cb592f
vincebarresi <vincebarresi@gmail.com>
2016-03-13 13:04:14 -0400

    initial commit

---
+++ swift/cap-and-reverse-alternate.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// O(n^2)
+func capAndReverseAlternate(str: String) -> String {
+
+    let arr_string = str.componentsSeparatedByString(" ")
+    var i = 0
+    var newArray = Array<String>()
+    
+    for word in arr_string {
+        
+        if i % 2 == 0 {
+            newArray.insert(word.uppercaseString, atIndex: i)
+        } else {
+            newArray.insert(String(word.characters.reverse()), atIndex: i)
+        }
+        i++
+    }
+    
+    return newArray.joinWithSeparator(" ")
+}
